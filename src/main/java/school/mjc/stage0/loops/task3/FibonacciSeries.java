f46f512a706cccbd3911b49d52416b5f35a206a6
Darkhan Zhursin <daratechn@gmail.com>
2024-01-30 06:46:27 -0600

    done

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int n1 = 0;
+        int n2 = 1;
+        for (int i = 1; i <= lastFibonacci; i++) {
+            System.out.println(n1);
+            int nextTerm = n1 + n2;
+            n1 = n2;
+            n2 = nextTerm;
+        }
     }
 }
